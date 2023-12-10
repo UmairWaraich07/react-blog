0ac4e19d02e84c49818dc3debf16347d673ea68c
Umair <umairwaraich2019@gmail.com>
2023-12-10 22:53:38 +0500

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <h1 className="text-2xl text-center ">Lets go React.js</h1>
+    </>
+  );
+}
+
+export default App;
