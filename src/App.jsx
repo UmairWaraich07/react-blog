f2e76844b882439410cd9e85460472de5dbd12dc
Umair <umairwaraich2019@gmail.com>
2023-12-11 21:34:39 +0500

    appwrite setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1 className="text-2xl text-center ">Lets go React.js</h1>
-    </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getLoggedInUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex content-between bg-slate-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+          Lets go React.js
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading...</div>
   );
 }
 
